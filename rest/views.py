e4456bd070092b149200d183797d447cd5cfae59
montenegrop <pablo.rpm@gmail.com>
2022-05-22 20:47:40 -0300

    fix

--- rest/views.py
+++ rest/views.py
@@ -2,17 +2,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 import json
-import os
 
-print(os.getcwd())
 
-# corregir: ver el path
-
-f = open("rest/dummy_data/request1.json", "r")
+f = open("rest/dummy_data/dama_muerta_1.json", "r")
 data = json.load(f)
 f.close()
+
 
 @api_view()
 def dummy_casino_json(request):
     return Response(data)
-    
