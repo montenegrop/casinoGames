5680422bd5992af7fcc44374f3af261f4943e547
montenegrop <pablo.rpm@gmail.com>
2022-05-30 08:59:03 -0300

    config-psg-machine

--- machine/models.py
+++ machine/models.py
@@ -1,6 +1,8 @@
+from re import T
 from django.db import models
 
 from machine.computations.examples import victorious_payment
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
@@ -8,6 +10,14 @@ from machine.computations.examples import victorious_payment
 class Machine(models.Model):
     # identificar la maquina que estamos usando
     configuration = models.CharField(max_length=10000, default="")
+    reels_round = ArrayField(
+        models.CharField(max_length=200, blank=True),
+        size=5,
+    )
+    payments = models.JSONField()
+    free_spins = ArrayField(models.IntegerField(blank=True),
+                            size=5)
+
     # corregir:
     # ver si se puede mejorar
 
