13b7cb0e54de5cc65a3643d72e3806e3b46cc27e
montenegrop <pablo.rpm@gmail.com>
2022-06-03 11:34:52 -0300

    roi exhaustive

---
+++ machine/computations/partial_roi/time_decorator.py
@@ -0,0 +1,17 @@
+from functools import wraps
+import time
+
+
+def timeit(param):
+
+    def inner_function(func):
+        @wraps(func)
+        def timeit_wrapper(*args, **kwargs):
+            start_time = time.perf_counter()
+            result = func(*args, **kwargs)
+            end_time = time.perf_counter()
+            total_time = end_time - start_time
+            print(
+                f'Inicial: {param} Tomó {total_time:.4f} segundos')
+        return timeit_wrapper
+    return inner_function
